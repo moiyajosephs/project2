9b802b205e93b7dfda3bd3c52d5945d89f0aedbf
mike hill <michael.hill.gd@outlook.com>
2021-11-19 14:27:55 -0500

    i have no what it thinks i changed

--- src/main/scala/admin.scala
+++ src/main/scala/admin.scala
@@ -98,19 +98,4 @@ object admin {
 
   }
 
-
-  //creates spark session
-  System.setProperty("hadoop.home.dir", "C:\\Program Files (x86)\\hadoop")
-  val spark = SparkSession
-    .builder
-    .appName("hello hive")
-    .config("spark.master", "local")
-    .enableHiveSupport()
-    .getOrCreate()
-  println("created spark session")
-  spark.sparkContext.setLogLevel("ERROR")
-  Logger.getLogger("org").setLevel(Level.OFF)
-  Logger.getLogger("akka").setLevel(Level.OFF)
-  Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
-
 }
